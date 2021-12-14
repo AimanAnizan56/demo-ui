0ab33bc0ac8c06c3c8826d95f8deda873f694379
Aiman Anizan <anizanaimann1@gmail.com>
2021-12-14 12:06:32 +0800

    Add loginServlet.java

---
+++ src/main/java/demoui/controller/loginServlet.java
@@ -0,0 +1,22 @@
+package demoui.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", value = "/loginServlet")
+public class loginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = (String) request.getParameter("username");
+        String password = (String) request.getParameter("password");
+
+
+    }
+}
