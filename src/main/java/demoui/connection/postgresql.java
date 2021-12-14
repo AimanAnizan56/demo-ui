bd258a1b4c6049e4ebe3349d6bc83be76fe3dc67
Aiman Anizan <anizanaimann1@gmail.com>
2021-12-14 12:56:15 +0800

    Update postgresql.java

--- src/main/java/demoui/connection/postgresql.java
+++ src/main/java/demoui/connection/postgresql.java
@@ -1,21 +1,27 @@
 package demoui.connection;
 
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class postgresql {
     private final String postgresql_driver = "org.postgresql.Driver";
-    private final String postgresql_url = "postgres://hpkmourdokyitg:385268bfad6244b6512f5c88c254b87fe8efd8f22f3bcb86dd4f33e14c4f134a@ec2-54-204-128-96.compute-1.amazonaws.com:5432/d4h4k0j9uun7dk";
-    private final String postgresql_host = "ec2-54-204-128-96.compute-1.amazonaws.com";
-    private final String postgresql_user = "hpkmourdokyitg";
-    private final String postgresql_password = "385268bfad6244b6512f5c88c254b87fe8efd8f22f3bcb86dd4f33e14c4f134a";
+    private URI dbUri = null;
 
     private static Connection conn = null;
 
     private postgresql() {
         try {
             Class.forName(postgresql_driver);
-            conn = DriverManager.getConnection(postgresql_url);
+
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            conn = DriverManager.getConnection(dbUrl, username, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
