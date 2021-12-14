ec505f572ac45bd3d9ab392b8ed24584b845eb4b
Aiman Anizan <anizanaimann1@gmail.com>
2021-12-14 12:44:45 +0800

    Update loginServlet.java

--- src/main/java/demoui/controller/loginServlet.java
+++ src/main/java/demoui/controller/loginServlet.java
@@ -23,17 +23,21 @@ public class loginServlet extends HttpServlet {
 
 
         try {
-            Connection conn = postgresql.getConnection();
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
 
+            Connection conn = postgresql.getConnection();
+            if(conn == null) {
+                out.println("<h1>Cannot establish connection</h1>");
+                out.println("</body></html>");
+            }
             PreparedStatement prepStmt = conn.prepareStatement("SELECT username FROM account WHERE username=? AND password=?");
             prepStmt.setString(1,username);
             prepStmt.setString(2,password);
 
             ResultSet result = prepStmt.executeQuery();
 
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-            out.println("<html><body>");
 
             if (result.getRow() == 1) {
                 String currentUsername = result.getString(1);
